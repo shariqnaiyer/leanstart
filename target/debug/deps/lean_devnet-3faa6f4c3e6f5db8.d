2c4c4f293ed46a88e5fe0deda07ee646f22ea12b
Shariq Naiyer <shariqnaiyer@gmail.com>
2026-04-14 21:50:09 -0600

    feat: devnet 4

--- target/debug/deps/lean_devnet-3faa6f4c3e6f5db8.d
+++ target/debug/deps/lean_devnet-3faa6f4c3e6f5db8.d
@@ -1,8 +1,8 @@
-/Users/shariqnaiyer/Documents/dev/lean-devnet/target/debug/deps/lean_devnet-3faa6f4c3e6f5db8.d: src/lib.rs src/config/mod.rs src/config/clients.rs src/config/generator.rs src/config/spec.rs src/genesis/mod.rs src/genesis/runner.rs src/k8s/mod.rs src/k8s/values.rs src/keys/mod.rs src/keys/keygen.rs
+/Users/shariqnaiyer/Documents/dev/leanstart/target/debug/deps/lean_devnet-3faa6f4c3e6f5db8.d: src/lib.rs src/config/mod.rs src/config/clients.rs src/config/generator.rs src/config/spec.rs src/genesis/mod.rs src/genesis/runner.rs src/k8s/mod.rs src/k8s/values.rs src/keys/mod.rs src/keys/keygen.rs
 
-/Users/shariqnaiyer/Documents/dev/lean-devnet/target/debug/deps/liblean_devnet-3faa6f4c3e6f5db8.rlib: src/lib.rs src/config/mod.rs src/config/clients.rs src/config/generator.rs src/config/spec.rs src/genesis/mod.rs src/genesis/runner.rs src/k8s/mod.rs src/k8s/values.rs src/keys/mod.rs src/keys/keygen.rs
+/Users/shariqnaiyer/Documents/dev/leanstart/target/debug/deps/liblean_devnet-3faa6f4c3e6f5db8.rlib: src/lib.rs src/config/mod.rs src/config/clients.rs src/config/generator.rs src/config/spec.rs src/genesis/mod.rs src/genesis/runner.rs src/k8s/mod.rs src/k8s/values.rs src/keys/mod.rs src/keys/keygen.rs
 
-/Users/shariqnaiyer/Documents/dev/lean-devnet/target/debug/deps/liblean_devnet-3faa6f4c3e6f5db8.rmeta: src/lib.rs src/config/mod.rs src/config/clients.rs src/config/generator.rs src/config/spec.rs src/genesis/mod.rs src/genesis/runner.rs src/k8s/mod.rs src/k8s/values.rs src/keys/mod.rs src/keys/keygen.rs
+/Users/shariqnaiyer/Documents/dev/leanstart/target/debug/deps/liblean_devnet-3faa6f4c3e6f5db8.rmeta: src/lib.rs src/config/mod.rs src/config/clients.rs src/config/generator.rs src/config/spec.rs src/genesis/mod.rs src/genesis/runner.rs src/k8s/mod.rs src/k8s/values.rs src/keys/mod.rs src/keys/keygen.rs
 
 src/lib.rs:
 src/config/mod.rs:
